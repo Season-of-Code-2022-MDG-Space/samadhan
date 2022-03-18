5f9da496043b7bec85784a0d76190b7cd72aba93
Chinmay Nagpal <c_nagpal@ch.iitr.ac.in>
2022-03-18 17:18:58 +0530

    Adds some features related to firebase authentication and CRUD, also adds others page

---
+++ lib/functions/database_crud.dart
@@ -0,0 +1,64 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+CollectionReference users = FirebaseFirestore.instance.collection('Users');
+create(String docName, name, emailid, bhawan, phoneno, enrolmentno) async {
+  await FirebaseFirestore.instance.collection('Users').doc(docName).set({
+    'Name': name,
+    'Email Id': emailid,
+    'Bhawan Name': bhawan,
+    'Enrolment Number': enrolmentno,
+    'Phone Number': phoneno,
+  }).then((value) => print("User signed up successfully"));
+}
+
+CollectionReference Icomplaints = FirebaseFirestore.instance.collection('Internet Complaints');
+createIC(String name, bhawan, phoneno, complaint) async {
+  await FirebaseFirestore.instance.collection('Internet Complaints').doc().set({
+    'Name': name,
+    'Bhawan Name': bhawan,
+    'Phone Number': phoneno,
+    'Complaint': complaint,
+  }).then((value) => print("Internet Complaint registered successfully"));
+}
+
+CollectionReference Ecomplaints = FirebaseFirestore.instance.collection('Electricity Complaints');
+createEC(String name, bhawan, phoneno, complaint) async {
+  await FirebaseFirestore.instance.collection('Water Complaints').doc().set({
+    'Name': name,
+    'Bhawan Name': bhawan,
+    'Phone Number': phoneno,
+    'Complaint': complaint,
+  }).then((value) => print("Electricity Complaint registered successfully"));
+}
+
+CollectionReference Wcomplaints = FirebaseFirestore.instance.collection('Water Complaints');
+createWC(String name, bhawan, phoneno, complaint) async {
+  await FirebaseFirestore.instance.collection('Water Complaints').doc().set({
+    'Name': name,
+    'Bhawan Name': bhawan,
+    'Phone Number': phoneno,
+    'Complaint': complaint,
+  }).then((value) => print("Water related Complaint registered successfully"));
+}
+
+CollectionReference Fcomplaints = FirebaseFirestore.instance.collection('Food Complaints');
+createFC(String name, bhawan, phoneno, complaint) async {
+  await FirebaseFirestore.instance.collection('Food Complaints').doc().set({
+    'Name': name,
+    'Bhawan Name': bhawan,
+    'Phone Number': phoneno,
+    'Complaint': complaint,
+  }).then((value) => print("Food Complaint registered successfully"));
+}
+
+CollectionReference othercomplaints = FirebaseFirestore.instance.collection('Other Complaints');
+createOC(String name, bhawan, phoneno, complaint) async {
+  await FirebaseFirestore.instance.collection('Other Complaints').doc().set({
+    'Name': name,
+    'Bhawan Name': bhawan,
+    'Phone Number': phoneno,
+    'Complaint': complaint,
+  }).then((value) => print("Complaint registered successfully"));
+}
